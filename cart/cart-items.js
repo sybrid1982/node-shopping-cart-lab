105e228ad280eb43a8764c4289229c842999ebed
Sydney Bridges <sybrid@gmail.com>
2018-08-28 15:37:47 -0400

    Building out files

---
+++ cart/cart-items.js
@@ -0,0 +1,22 @@
+cart = [
+    {
+        id: 0,
+        product: "Hat",
+        price: 10,
+        quantity: 1,
+    },
+    {
+        id: 1,
+        product: "Canned Cat Food",
+        price: .50,
+        quantity: 12,
+    },
+    {
+        id: 2,
+        product: "Milk",
+        price: 3,
+        quantity: 1,
+    }
+];
+
+module.export = cart;
